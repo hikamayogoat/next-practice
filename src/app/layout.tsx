6ddd98be0ac98723c59638196d0bbc6709d34325
hikamayogoat <hikamayo.dw@gmail.com>
2023-02-26 21:03:37 +0900

    テーブルを反復で作るには

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const rowCells = new Array<number>(10).fill(0);
+  const columnCells = new Array<number>(10).fill(0);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <h2>テトリス譜 製作中</h2>
-      </div>
+      <p> should shown a table below </p>
+      <table border={1}>
+        {
+          columnCells.map((col) =>
+          <tr>
+            {
+              rowCells.map((row) => 
+                <td>0</td>
+              )
+            }
+          </tr>
+          )
+        }
+      </table>
     </main>
   );
 }
